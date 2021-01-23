0a7c05589a31f44eba0cb7cb5049393a709e39d6
dongyeol <eastheat10@gmail.com>
2021-01-23 23:46:36 +0900

    fail

---
+++ src/algo/stage11/A7568.java
@@ -0,0 +1,28 @@
+package algo.stage11;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class A7568 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuffer sb = new StringBuffer();
+		int n = Integer.parseInt(br.readLine().trim());
+
+		int[][] info = new int[n][];
+		int[] rank = new int[n];
+
+		for(int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			info[i][0] = Integer.parseInt(st.nextToken());
+			info[i][1] = Integer.parseInt(st.nextToken());
+		}
+	}
+
+}
