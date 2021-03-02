509ab54217ebdce987fe5318d1e425fa454e1d75
dongyeol <eastheat10@gmail.com>
2021-03-03 00:03:19 +0900

    별 찍 기

--- src/algo/stage10/A2447.java
+++ src/algo/stage10/A2447.java
@@ -1,35 +1,53 @@
 package algo.stage10;
 
+import javax.lang.model.type.ArrayType;
 import java.io.*;
+import java.util.Arrays;
 
 public class A2447 {
+	public static StringBuffer sb1 = new StringBuffer();
 
-	static String star ="*";
-	static int count = 0, height = 0;
-	static StringBuffer sb = new StringBuffer();
+	static char[][] arr;
+	static int count = 0;
 
-	public static String printStar(int n) {
-		if(count == 1){
-			if (height != 2) {
-				sb.append(star + star + star + "\n");
-				if(height == 3) height = 0;
-				star = sb.toString();
-			}
-			else
-				sb.append(star + " " + star + "\n");
-		} else {
-			count++;
-			printStar(n - count);
+	public static void printStar(int x, int y, int n) {
+		if(n == 1){
+			arr[x][y] = '*';
+			return;
 		}
-		return star;
+
+		int tmp = n / 3;
+
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (!(i == 1 && j == 1)) {
+					printStar(x + (i * tmp), y + (j * tmp), tmp);
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuffer sb = new StringBuffer();
 		int n = Integer.parseInt(br.readLine());
 
-		printStar(n);
+		arr = new char[n][n];
+		for (int k = 0; k < n; k++) {
+			Arrays.fill(arr[k],' ');
+		}
+
+		int x = 0, y = 0;
+
+		printStar(x, y, n);
+
+		for (char[] tmp1 : arr) {
+			for (char tmp2 : tmp1) {
+				sb.append(tmp2);
+			}
+			sb.append("\n");
+		}
 
 		bw.write(sb.toString());
 		bw.flush();bw.close();
