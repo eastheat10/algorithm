78fb42cf3936ce5c05757c69f36b5c2dee767ae7
dongyeol <eastheat10@gmail.com>
2021-03-05 00:32:15 +0900

    2021 1학기 알고리즘 과제

---
+++ src/homework/hw0_1/Hello.java
@@ -0,0 +1,8 @@
+package homework.hw0_1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hw0_1 : 윤동열");
+        System.out.println("Hello World!");
+    }
+}
