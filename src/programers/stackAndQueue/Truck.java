abb7edbe3e22e6bc2fcb1e748ba75196be00090e
dongyeol <eastheat10@gmail.com>
2021-04-22 16:20:50 +0900

    다리를 지나는 트럭

--- src/programers/stackAndQueue/Truck.java
+++ src/programers/stackAndQueue/Truck.java
@@ -1,56 +1,50 @@
 package programers.stackAndQueue;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
+// 다리를 지나는 트럭
 public class Truck {
 
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
-        int time = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(truck_weights[0]);
-        int tmpWeight = truck_weights[0];
-        int index = 0;  // 선두 트럭 인덱스
+        int answer = 0;
+        Queue<Integer> bridge = new LinkedList<>();  // 다리
+        int currentWeight = 0;
 
-        for (int i = 1; i < truck_weights.length; i++) {
-            if(tmpWeight + truck_weights[i] > weight) {
-                while (tmpWeight + truck_weights[i] > weight) {
-                    time += bridge_length;
-                    q.poll();
-                    tmpWeight -= truck_weights[index];
-                    index++;
+        for (int truck : truck_weights) {
+            while (true) {
+                if (bridge.isEmpty()) {
+                    bridge.add(truck);
+                    currentWeight += truck;
+                    answer++;
+                    break;
+                } else if (bridge.size() == bridge_length) {
+                    currentWeight -= bridge.poll();
+                } else {
+                    if (truck + currentWeight > weight) {
+                        bridge.offer(0);
+                        answer++;
+                    } else {
+                        bridge.offer(truck);
+                        answer++;
+                        currentWeight += truck;
+                        break;
+                    }
                 }
             }
-            else {
-                q.add(truck_weights[i]);
-                tmpWeight += truck_weights[i];
-
-            }
         }
-        time += bridge_length;
 
-        return time;
+        return answer + bridge_length;
     }
 
     public static void main(String[] args) {
-
-        int weight = 10;
-//        int weight = 100;
-//        int weight = 100;
-
-        int bridge_length = 2;
-//        int bridge_length = 100;
-//        int bridge_length = 100;
-
-
-        int[] truck_weight = {7, 4, 5, 6};
-//        int[] truck_weight = {10};
-//        int[] truck_weight = {10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
-
-        int result = solution(weight, bridge_length, truck_weight);
-
-        System.out.println("result = " + result);
-
+        System.out.println("answer = " + solution(2, 10, new int[]{7, 4, 5, 6}));
+        System.out.println("answer = " + solution(100, 10, new int[]{10}));
+        System.out.println("answer = " + solution(100, 100, new int[]{10,10,10,10,10,10,10,10,10,10}));
     }
+
+//    bridge_length	weight	truck_weights	                 return
+//    2	            10	    [7,4,5,6]	                     8
+//    100	        100	    [10]	                         101
+//    100	        100	    [10,10,10,10,10,10,10,10,10,10]	 110
 }
