b5a1ba101ae6aafaacc25d7f85f6e70ff72f0d42
dongyeol <eastheat10@gmail.com>
2021-04-25 15:34:30 +0900

    전화번호부

--- src/programers/hash/Phone_book.java
+++ src/programers/hash/Phone_book.java
@@ -1,51 +1,54 @@
 package programers.hash;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
+// 전화번호 목록
 public class Phone_book {
 
-		/*
-		boolean result = true;
-		int size = phone_book.length;
-		for(int i = 0; i < size - 1; i++){
-			for(int j = i + 1; j < size; j++){
-				System.out.println("phone_book[i] = " + phone_book[i]);
-				System.out.println("phone_book[j] = " + phone_book[j]);
-				if(phone_book[j].startsWith(phone_book[i])){
+    public static boolean solution(String[] phone_book) {
+    	int len = phone_book.length;
+		HashSet<String> hs = new HashSet<>();
+//        HashMap<Integer, String> hm = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+//			hm.put(i, phone_book[i]);
+			hs.add(phone_book[i]);
+		}
+
+		Iterator<String> iter = hs.iterator();
+		while (iter.hasNext()) {
+			String s = iter.next();
+			for (int i = 0; i < s.length(); i++) {
+				if (hs.contains(s.substring(0, i))) {
 					return false;
 				}
 			}
 		}
 		return true;
-*/
-		public static boolean solution(String[] phone_book){
-			HashMap<Integer, String> hm = new HashMap<>();
-			int min = 1000000;
-			for(int i = 0; i < phone_book.length; i++){
-				if(phone_book[i].length() < min)
-					min = phone_book[i].length();
-				hm.put(i, phone_book[i]);
-			}
 
-			for(int i = 0; i < hm.size(); i++){
-				String number = hm.get(i);
-				hm.remove(i);
-					for (int j = 0; j < min + 1; j++) {
-						if (hm.containsValue(number.substring(0, j))) {
-							return false;
-						}
-					}
-				hm.put(i, number);
-			}
-			return true;
-		}
+//		for (int i = 0; i < hm.size(); i++) {
+//            String number = hm.get(i);
+//            hm.remove(i);
+//            for (int j = 0; j < len + 1; j++) {
+//                if (hm.containsValue(number.substring(0, j))) {
+//                    return false;
+//                }
+//            }
+//            hm.put(i, number);
+//        }
+//        return true;
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-//		 String[] phone_book = {"119", "97674223", "1195524421"};
-		 String[] phone_book = {"123", "456", "789"};
-//		 String[] phone_book = {"12", "213", "1235", "567", "88"};
-		System.out.println(solution(phone_book));
+        String[] phone_book1 = {"119", "97674223", "1195524421"};
+        String[] phone_book2 = {"123", "456", "789"};
+        String[] phone_book3 = {"12", "213", "1235", "567", "88"};
 
-	}
+        System.out.println(solution(phone_book1));
+        System.out.println(solution(phone_book2));
+        System.out.println(solution(phone_book3));
+
+    }
 }
