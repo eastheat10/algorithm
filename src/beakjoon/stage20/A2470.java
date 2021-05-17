2ba0e24545d1cba6b75176563ea4d8e668aaf3c1
dongyeol <eastheat10@gmail.com>
2021-05-18 01:16:27 +0900

    binary search

--- src/beakjoon/stage20/A2470.java
+++ src/beakjoon/stage20/A2470.java
@@ -1,23 +1,22 @@
 package beakjoon.stage20;
 
+import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
-// 두 용액
-public class A2470 {
-    static StringBuilder sb = new StringBuilder();
-    static int num;
+class A2470{
+    static int n;
     static int[] solution;
+    static StringBuilder sb = new StringBuilder();
 
-    static int count(int l, int r, int x) {
+    static int count(int l, int r, int x){
         int result = r + 1;
 
-        while (l <= r) {
+        while(l <= r){
             int mid = (l + r) / 2;
-            if (solution[mid] >= x) {
+            if(solution[mid] >= x){
                 result = mid;
                 r = mid - 1;
             } else {
@@ -28,41 +27,39 @@ public class A2470 {
         return result;
     }
 
-    static void find() {
+    static void find(){
         int bestSum = Integer.MAX_VALUE;
         int r1 = 0;
         int r2 = 0;
+        for(int l = 1; l <= n; l++){
+            int boundary = count(l + 1, n, -solution[l]);
 
-        for (int i = 1; i <= num / 2; i++) {
-            int boundary = count(i + 1, num, -solution[i]);
-
-            if (boundary >= i + 1 && boundary < num + 1 && Math.abs(solution[i] + solution[boundary]) < bestSum) {
-                bestSum = Math.abs(solution[i] + solution[boundary]);
-                r1 = solution[i];
+            if(boundary >= l + 1 && boundary <= n && Math.abs(solution[l] + solution[boundary]) < bestSum){
+                bestSum = Math.abs(solution[l] + solution[boundary]);
+                r1 = solution[l];
                 r2 = solution[boundary];
             }
-            if (boundary - 1 >= i + 1 && boundary - 1 < num + 1 && Math.abs(solution[i] + solution[boundary - 1]) < bestSum) {
-                bestSum = Math.abs(solution[i] + solution[boundary - 1]);
-                r1 = solution[i];
+            if(boundary - 1 >= l + 1 && boundary - 1 <= n && Math.abs(solution[l] + solution[boundary - 1]) < bestSum){
+                bestSum = Math.abs(solution[l] + solution[boundary - 1]);
+                r1 = solution[l];
                 r2 = solution[boundary - 1];
             }
         }
         sb.append(r1).append(" ").append(r2).append("\n");
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        num = Integer.parseInt(br.readLine());
-        solution = new int[num + 1];
+        n = Integer.parseInt(br.readLine());
+        solution = new int[n + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 1; i <= num; i++) {
+        for(int i = 1; i <= n; i++)
             solution[i] = Integer.parseInt(st.nextToken());
-        }
 
-        Arrays.sort(solution, 1, num + 1);
+        Arrays.sort(solution, 1, n + 1);
         find();
+
         System.out.println(sb.toString());
     }
 }
