d33b5c0e8e85f8061668c0a126829031921e73f4
dongyeol <eastheat10@gmail.com>
2021-05-18 00:47:04 +0900

    binary search

--- src/beakjoon/stage20/A2470.java
+++ src/beakjoon/stage20/A2470.java
@@ -34,17 +34,17 @@ public class A2470 {
         int r2 = 0;
 
         for (int i = 1; i <= num / 2; i++) {
-            int bound = count(i + 1, num, -solution[i]);
+            int boundary = count(i + 1, num, -solution[i]);
 
-            if (bound >= i + 1 && bound < num + 1 && Math.abs(solution[i] + solution[bound]) < bestSum) {
-                bestSum = Math.abs(solution[i] + solution[bound]);
+            if (boundary >= i + 1 && boundary < num + 1 && Math.abs(solution[i] + solution[boundary]) < bestSum) {
+                bestSum = Math.abs(solution[i] + solution[boundary]);
                 r1 = solution[i];
-                r2 = solution[bound];
+                r2 = solution[boundary];
             }
-            if (bound - 1 >= i + 1 && bound - 1 < num + 1 && Math.abs(solution[i] + solution[bound - 1]) < bestSum) {
-                bestSum = Math.abs(solution[i] + solution[bound - 1]);
+            if (boundary - 1 >= i + 1 && boundary - 1 < num + 1 && Math.abs(solution[i] + solution[boundary - 1]) < bestSum) {
+                bestSum = Math.abs(solution[i] + solution[boundary - 1]);
                 r1 = solution[i];
-                r2 = solution[bound - 1];
+                r2 = solution[boundary - 1];
             }
         }
         sb.append(r1).append(" ").append(r2).append("\n");
