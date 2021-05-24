ba694c2a4c1de37a70c237d58662ba83a0343187
dongyeol <eastheat10@gmail.com>
2021-05-24 18:22:43 +0900

    정수론과 조합론

--- src/beakjoon/numbertheoryandcombinatorics/A11051.java
+++ src/beakjoon/numbertheoryandcombinatorics/A11051.java
@@ -1,29 +1,33 @@
-package beakjoon.stage16;
+package beakjoon.numbertheoryandcombinatorics;
 
 import java.io.*;
 import java.util.StringTokenizer;
 
 // 이항계수2
 public class A11051 {
-    public static void main(String[] args) throws IOException {
+
+    static int n, k;
+    static int[][] com;
+
+    public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        int multiple = 1;
-        int tmp = n;
-        for(int i = k; i > 0; i--)
-            multiple *= tmp--;
-        for (int j = k; j > 0; j--)
-            multiple /= j;
-
-        multiple %= 10007;
-
-        bw.write(Integer.toString(multiple));
-        bw.flush(); bw.close(); br.close();
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        com = new int[n + 1][k + 1];
     }
+
+    static int combination(int n, int r) {
+        if(com[n][r] > 0)
+            return com[n][r];
+        if (n == r || r == 0)
+            return com[n][r] = 1;
+        return com[n][r] = combination(n - 1, r - 1) + combination(n - 1, r) % 10007;
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        System.out.println(combination(n, k));
+    }
+
 }
