1f7a0d0f242a224b82aacbf8dfbb036fb506dc10
dongyeol <eastheat10@gmail.com>
2021-06-20 00:48:42 +0900

    binary search

---
+++ src/beakjoon/binarysearch/A6236.java
@@ -0,0 +1,64 @@
+package beakjoon.binarysearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// 용돈 관리
+public class A6236 {
+
+    static int n, m;
+    static int[] arr;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+    }
+
+    static boolean check(int l) {
+        int count = 1;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += arr[i];
+            if(sum > l) {
+                count++;
+                sum = arr[i];
+            }
+        }
+
+        return count <= m;
+    }
+
+    static void process() {
+        Arrays.sort(arr, 1, n + 1);
+        int l = arr[1];
+        int r = arr[n] * m;
+        int answer = 0;
+
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (check(mid)) {
+                answer = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    public static void main(String[] args) throws IOException {
+        input();
+        process();
+    }
+
+}
