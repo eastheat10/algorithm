b2bd7fb79fa4fc1750e996f929274009d9c1d3bf
dongyeol <eastheat10@gmail.com>
2021-08-03 23:53:50 +0900

    N으로 표현

---
+++ src/programers/dp/ToN.java
@@ -0,0 +1,15 @@
+package programers.dp;
+
+// N으로 표현
+public class ToN {
+
+    public static int solution(int N, int number) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(5, 12));
+        System.out.println(solution(2, 11));
+    }
+}
