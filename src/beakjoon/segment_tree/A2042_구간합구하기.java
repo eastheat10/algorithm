6e3807e049c1daf882efb5f9f0b3a651257dde46
eastheat10 <eastheat10@gmail.com>
2022-02-08 14:56:42 +0900

    segment tree

--- src/beakjoon/segment_tree/A2042_구간합구하기.java
+++ src/beakjoon/segment_tree/A2042_구간합구하기.java
@@ -1,2 +1,90 @@
-package beakjoon.segment_tree;public class A2042_구간합구하기 {
+package beakjoon.segment_tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A2042_구간합구하기 {
+
+    static int n, m, k, s = 1;
+    static long[] arr, tree;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new long[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Long.parseLong(br.readLine());
+        }
+
+        while (s < n) {
+            s <<= 1;
+        }
+
+        tree = new long[s * 2];
+
+        init(1, 1, n);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m + k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
+
+            if (a == 1) {
+                long diff = c - arr[b];
+                arr[b] = c;
+                update(1, 1, n, b, diff);
+            } else if (a == 2) {
+                sb.append(query(1, 1, n, b, (int) c)).append('\n');
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static long init(int node, int start, int end) {
+        if (start == end) {
+            return tree[node] = arr[start];
+        }
+
+        int mid = (start + end) / 2;
+        return tree[node] = init((node * 2), start, mid) +
+                init((node * 2) + 1, mid + 1, end);
+    }
+
+    static void update(int node, int start, int end, int targetIdx, long diff) {
+        if (targetIdx < start || end < targetIdx){
+            return;
+        }
+
+        tree[node] += diff;
+        if (start == end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        update((node * 2), start, mid, targetIdx, diff);
+        update((node * 2) + 1, mid + 1, end, targetIdx, diff);
+    }
+
+    static long query(int node, int start, int end, int targetLeft, int targetRight) {
+        if (targetRight < start || end < targetLeft) {
+            return 0;
+        }
+
+        if (targetLeft <= start && end <= targetRight) {
+            return tree[node];
+        }
+
+        int mid = (start + end) / 2;
+        return query((node * 2), start, mid, targetLeft, targetRight) +
+                query((node * 2) + 1, mid + 1, end, targetLeft, targetRight);
+    }
 }
--- src/beakjoon/segment_tree/A2243_사탕상자.java
+++ src/beakjoon/segment_tree/A2243_사탕상자.java
@@ -1,2 +1,74 @@
-package beakjoon.segment_tree;public class A2243_사탕상자 {
+package beakjoon.segment_tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A2243_사탕상자 {
+
+    final static int MAX = 1_000_000;
+    static int n, s = 1;
+    static int a, b;
+    static long c;
+    static int[] tree;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        while (s < MAX) {
+            s <<= 1;
+        }
+        tree = new int[2 * s];
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            a = Integer.parseInt(st.nextToken());
+
+            if (a == 1) {
+                b = Integer.parseInt(st.nextToken());
+                int index = query(1, 1, MAX, b);
+                update(1, 1, MAX, index, -1);
+                sb.append(index).append('\n');
+            } else if (a == 2) {
+                b = Integer.parseInt(st.nextToken());
+                c = Long.parseLong(st.nextToken());
+                update(1, 1, MAX, b, c);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    static void update(int node, int start, int end, int targetIdx, long diff) {
+        if (start > targetIdx || end < targetIdx)
+            return;
+
+        tree[node] += diff;
+        if (start == end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        update((node * 2), start, mid, targetIdx, diff);
+        update((node * 2) + 1, mid + 1, end, targetIdx, diff);
+    }
+
+    static int query(int node, int start, int end, int rank) {
+        if (start == end)
+            return start;
+
+        int mid = (start + end) / 2;
+
+        if (tree[node * 2] >= rank) {
+            return query(node * 2, start, mid, rank);
+        } else {
+            return query((node * 2) + 1, mid + 1, end, rank - tree[node * 2]);
+        }
+    }
+
 }
