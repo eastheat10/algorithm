b28a367576d017db9de60ff3e37e1e1f0cf8b956
Dongyeol <eastheat10@gmail.com>
2023-06-15 19:30:48 +0900

    Programmers 순위 검색

---
+++ src/main/java/programers/etc/RotateMatrixBorders.java
@@ -0,0 +1,12 @@
+package programers.etc;
+
+public class RotateMatrixBorders {
+
+    public int[] solution(int rows, int columns, int[][] queries) {
+
+        int[][] arr = new int[rows + 1][columns + 1];
+
+        return null;
+    }
+
+}
--- src/main/java/programers/etc/SearchRanking.java
+++ src/main/java/programers/etc/SearchRanking.java
@@ -1,183 +1,60 @@
 package programers.etc;
 
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SearchRanking {
 
-    private List<Participant> list;
+    Map<String, List<Integer>> map;
 
     public int[] solution(String[] info, String[] query) {
-        int[] result = new int[query.length];
-        list = init(info);
-
-        for (int i = 0; i < query.length; i++) {
-
-            Set<Participant> langSet = new HashSet<>();
-            Set<Participant> positionSet = new HashSet<>();
-            Set<Participant> levelSet = new HashSet<>();
-            Set<Participant> soulFoodSet = new HashSet<>();
-            Set<Participant> scoreSet = new HashSet<>();
-            String[] qs = query[i].split(" ");
-
-            int cnt = 0;
-            for (String q : qs) {
-                if (q.equals("and")) continue;
-                if (q.trim().equals("-")) {
-                    cnt++;
-                    continue;
-                }
-
-                if (cnt == 0) {
-                    langSet = checkLanguage(q);
-                } else if (cnt == 1) {
-                    positionSet = checkPosition(q);
-                } else if (cnt == 2) {
-                    levelSet = checkLevel(q);
-                } else if (cnt == 3) {
-                    soulFoodSet = checkSoulFood(q);
-                } else if (cnt == 4) {
-                    scoreSet = checkScore(Integer.parseInt(q));
-                }
-
-                cnt++;
-            }
-
-            int count = 0;
-
-            Set<Participant> all = get();
-
-            for (Participant p : all) {
-                if (langSet.contains(p)
-                    && positionSet.contains(p)
-                    && levelSet.contains(p)
-                    && soulFoodSet.contains(p)
-                    && scoreSet.contains(p)) {
-                    count++;
-                }
-            }
-
-            result[i] = count;
-        }
-
-        return result;
-    }
-
-    private Set<Participant> checkLanguage(String lang) {
-        Set<Participant> set = get();
-        Set<Participant> copy = new HashSet<>();
-        for (Participant p : set) {
-            if (!lang.equals(p.lang)) {
-                copy.add(p);
-            }
-        }
-
-        for (Participant p : copy) {
-            set.remove(p);
-        }
-
-        return set;
-    }
-
-    private Set<Participant> checkPosition(String position) {
-        Set<Participant> set = get();
-        Set<Participant> copy = new HashSet<>();
-        for (Participant p : set) {
-            if (!position.equals(p.position)) {
-                copy.add(p);
-            }
-        }
-
-        for (Participant p : copy) {
-            set.remove(p);
-        }
-
-        return set;
-    }
-
-    private Set<Participant> checkLevel(String level) {
-        Set<Participant> set = get();
-        Set<Participant> copy = new HashSet<>();
-        for (Participant p : set) {
-            if (!level.equals(p.level)) {
-                copy.add(p);
-            }
-        }
-
-        for (Participant p : copy) {
-            set.remove(p);
-        }
-
-        return set;
-    }
-
-    private Set<Participant> checkSoulFood(String soulFood) {
-        Set<Participant> set = get();
-        Set<Participant> copy = new HashSet<>();
-        for (Participant p : set) {
-            if (!soulFood.equals(p.soulFood)) {
-                copy.add(p);
-            }
-        }
-
-        for (Participant p : copy) {
-            set.remove(p);
-        }
-
-        return set;
-    }
-
-    private Set<Participant> checkScore(int score) {
-        Set<Participant> set = get();
-        Set<Participant> copy = new HashSet<>();
-        for (Participant p : set) {
-            if (score > p.score) {
-                copy.add(p);
-            }
-        }
-
-        for (Participant p : copy) {
-            set.remove(p);
-        }
-
-        return set;
-    }
-
-    private List<Participant> init(String[] info) {
-        List<Participant> list = new ArrayList<>();
+        map = new HashMap<>();
+        int[] answer = new int[query.length];
 
         for (String i : info) {
-            String[] s = i.split(" ");
-            list.add(new Participant(s[0], s[1], s[2], s[3], Integer.parseInt(s[4])));
+            makeSentence(i.split(" "), "", 0);
         }
 
-        return list;
+        map.values().forEach(Collections::sort);
+
+        for (int i = 0; i < query.length; i++) {
+            String s = query[i].replaceAll(" and ", "");
+            String[] arr = s.split(" ");
+            answer[i] = map.containsKey(arr[0]) ? search(arr[0], Integer.parseInt(arr[1])) : 0;
+        }
+
+        return answer;
     }
 
-    private Set<Participant> get() {
-        Set<Participant> set = new HashSet<>();
-        for (Participant participant : list) {
-            set.add(participant);
+    private void makeSentence(String[] q, String s, int depth) {
+        if (depth == 4) {
+            map.computeIfAbsent(s, ignore -> new ArrayList<>());
+            map.get(s).add(Integer.parseInt(q[4]));
+            return;
         }
-        return set;
+
+        makeSentence(q, s + q[depth], depth + 1);
+        makeSentence(q, s + "-", depth + 1);
     }
 
-    static class Participant {
-        String lang;
-        String position;
-        String level;
-        String soulFood;
-        int score;
+    private int search(String key, int score) {
+        List<Integer> list = map.get(key);
+        int l = 0;
+        int r = list.size() - 1;
 
-        public Participant(String lang, String position, String level, String soulFood, int score) {
-            this.lang = lang;
-            this.position = position;
-            this.level = level;
-            this.soulFood = soulFood;
-            this.score = score;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (list.get(mid) < score) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
+
+        return list.size() - l;
     }
 
 }
